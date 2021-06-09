a952cea7ee3d81db8605bbf569edd5badc0c3f32
MatthiasvsGitHub <matthias@schmettow.de>
2021-06-09 14:00:54 +0200

    initial commit

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# Inquizable_Front
---
+++ src/Routing.js
@@ -0,0 +1,56 @@
+import { Switch, Route, Redirect } from 'react-router-dom'
+import {Home} from './components/Home'
+import {Login} from './components/Login'
+import {Admin} from './components/Admin'
+import {Account} from './components/Account'
+import {Categories} from './components/Categories'
+import {Quiz} from './components/Quiz'
+import {Contact} from './components/Contact'
+import {Submit} from './components/Submit'
+import {Leaderboard} from './components/Leaderboard'
+import {About} from './components/About'
+
+function Routing() {
+  return (
+    <div>
+      <h1 className='animate-bounce'>Hello World</h1>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+        <Route exact path="/account">
+          <Account />
+        </Route>
+        <Route exact path="/categories">
+          <Categories />
+        </Route>
+        <Route exact path="/quiz">
+          <Quiz />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/submit">
+          <Submit />
+        </Route>
+        <Route exact path="/leaderboard">
+          <Leaderboard />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default Routing;
