05a77c2f991c43542dd5fcd060855defb44cb1da
MatthiasvsGitHub <matthias@schmettow.de>
2021-06-17 16:50:37 +0200

    still shuffling issue once answered

--- src/components/QuizPart/Questions.js
+++ src/components/QuizPart/Questions.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './Questions.css';
 /* import {decode} from 'html-entities'; */
 import arrayShuffle from "array-shuffle";
 
 export const Questions = ({ question, questionIndex, setQuestionIndex }) => {
+  const [answerShowing, setAnswerShowing] = useState(false)
 
-  let answers = question.incorrect_answers
+  const answers = question.incorrect_answers
   if (answers.length < 4) answers.unshift(question.correct_answer)
-  let shuffledAnswers = arrayShuffle(answers)
+  const shuffledAnswers = arrayShuffle(answers)
+  console.log(shuffledAnswers)
+  const handleAnswer = (e) => {
+    if (e.target.innerHTML == question.correct_answer) alert('Correct!')
+    else alert('Wrong!')
+    setAnswerShowing(true)
+  }
 
-  const handleAnswer = () => {
+  const nextQuestion = () => {
+    setAnswerShowing(false)
     setQuestionIndex(() => questionIndex + 1)
   }
 
@@ -29,15 +37,16 @@ export const Questions = ({ question, questionIndex, setQuestionIndex }) => {
           <div>
             <div className="quest xs:text-center sm:text-center md:text-center mt-12 w-100 border-bottom">
               <h2>
-              {question.question}
+                {question.question}
               </h2>
             </div>
             <div className="select-answer md:mx-auto xs:text-center sm:text-center md:text-center mt-6 sm:block md:flex ">
-              <button onClick={handleAnswer} className="Questions-Box md:mr-4">{shuffledAnswers[0]}</button>
-              <button onClick={handleAnswer} className="Questions-Box md:mr-4">{shuffledAnswers[1]}</button>  
-              <button onClick={handleAnswer} className="Questions-Box md:mr-4">{shuffledAnswers[2]}</button>
-              <button onClick={handleAnswer} className="Questions-Box">{shuffledAnswers[3]}</button>
+              <button disabled={answerShowing} onClick={(e) => handleAnswer(e)} className="Questions-Box md:mr-4">{shuffledAnswers[0]}</button>
+              <button disabled={answerShowing} onClick={(e) => handleAnswer(e)} className="Questions-Box md:mr-4">{shuffledAnswers[1]}</button>
+              <button disabled={answerShowing} onClick={(e) => handleAnswer(e)} className="Questions-Box md:mr-4">{shuffledAnswers[2]}</button>
+              <button disabled={answerShowing} onClick={(e) => handleAnswer(e)} className="Questions-Box md:mr-4">{shuffledAnswers[3]}</button>
             </div>
+            {answerShowing && <button onClick={nextQuestion} className="Questions-Box">Next Question</button>}
           </div>
         </div>
         : "Loading..."}
