ed50ef11247d9b76f6c5b8abfeaccb98585fb512
Lodos <o.ueguer@gmail.com>
2021-06-18 18:47:49 +0200

    Heroicons added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import './Home.css';
 import Axios from 'axios';
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
 
 const User_DB = process.env.USERS;
@@ -24,6 +27,7 @@ export const Home = () => {
             <div className="xs:text-center sm:text-left bg-color">
             <h2 className="home-title">A fun learning platform where anyone can contribute</h2>
             </div>
+            
             
         </div>
     )
