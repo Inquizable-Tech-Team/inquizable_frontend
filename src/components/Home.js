979355d579414dfa09089d46feafde3420d107b7
Lodos <o.ueguer@gmail.com>
2021-06-18 18:24:47 +0200

    landing page designed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import './Home.css';
+import Axios from 'axios';
+
+
+const User_DB = process.env.USERS;
 
 
 export const Home = () => {
+    useEffect(() => {
+        fetchUsersData();
+      }, []);
+    
+      const fetchUsersData = async () => {
+        await Axios.get(User_DB)
+          .then((response) => console.log(response))
+          .catch((error) => console.log(error));
+      };
+
+
+
     return (
         <div>
-            Home
+            <div className="xs:text-center sm:text-left bg-color">
+            <h2 className="home-title">A fun learning platform where anyone can contribute</h2>
+            </div>
+            
         </div>
     )
 }
