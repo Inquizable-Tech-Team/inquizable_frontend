abd681a1bff0812734ab0150eaddfd6cbf752399
Lodos <o.ueguer@gmail.com>
2021-06-30 20:45:05 +0200

    account added

--- src/components/Account.js
+++ src/components/Account.js
@@ -102,15 +102,15 @@ export const Account = () => {
             >
               <polygon
                 className="text-gray-300 fill-current"
-                points="2560 0 2560 100 0 100"
+                points="2560 100 2560 100 100 100"
               ></polygon>
             </svg>
           </div>
         </section>
         {/* Account Content Section Start */}
-        <section className="relative py-16 bg-gray-300">
-          <div className="container mx-auto px-4">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+        <section className="relative py-16 Bg-Color">
+          <div className="container xs:mx-auto sm:mx-auto md:mx-auto px-4 xs:w-min-3/4 sm:w-4/5 md:w-4/5">
+            <div className="relative flex flex-col min-w-0 break-words bg-white xs:w-full sm:w-full md:w-full lg:w-full mb-6 shadow-xl rounded-lg -mt-64">
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
@@ -137,7 +137,7 @@ export const Account = () => {
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                    <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                    <div className="xs:block sm:block md:flex lg:flex justify-center py-4 lg:pt-4 pt-8">
                       <div className="mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
                           {details.answered ? details.answered : 0}
@@ -178,15 +178,15 @@ export const Account = () => {
                   </div>
                 </div>
                 <div className="mt-10 py-10 border-t border-gray-300 text-center">
-                  <div className="flex flex-wrap justify-center">
+                  <div className="flex flex-wrap justify-center xs:block sm:block md:flex lg:flex">
                     <div className="w-full lg:w-9/12 px-4">
                       {/* Changing User Info */}
-                      <form >
+                      <form className="xs:block sm:block md:flex lg:flex md:flex-wrap lg:flex-wrap">
                         <div className="mt-1">
                           <input onChange={e => setName(e.target.value.trim())} className="text-black appearance-none px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Change Nickname" />
                         </div>
                         <button
-                          className="bg-blue-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+                          className="xs:mt-1 bg-blue-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                           type="button"
                           style={{ transition: "all .15s ease" }}
                           onClick={updateName}
@@ -198,7 +198,7 @@ export const Account = () => {
                         </div>
                         <div>
                           <button
-                            className="bg-blue-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+                            className="xs:mt-1 bg-blue-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                             type="button"
                             style={{ transition: "all .15s ease" }}
                             onClick={updateMail}
@@ -214,7 +214,7 @@ export const Account = () => {
                         </div>
                         <div>
                           <button
-                            className="bg-blue-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+                            className="xs:mt-1 bg-blue-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                             type="button"
                             style={{ transition: "all .15s ease" }}
                             onClick={updatePassword}
