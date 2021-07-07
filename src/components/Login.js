4bda00df24f3f7b5dbed39499d375f5453219d35
Lodos <o.ueguer@gmail.com>
2021-07-07 11:41:00 +0200

    deleted double dots

--- src/components/Login.js
+++ src/components/Login.js
@@ -63,7 +63,7 @@ const register = (e) => {
         <div className="mt-8">
           <div className="mt-6">
             <form className="space-y-6" autoComplete="off" onSubmit={register} id='registerForm'>
-            <h2 className="xs:text-left sm:text-left md:text-left font-bold text-gray-700">You can register here:</h2>
+            <h2 className="xs:text-left sm:text-left md:text-left font-bold text-gray-700">You can register here</h2>
             <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">
               Name
@@ -93,7 +93,7 @@ const register = (e) => {
                 <input type="submit" value='Register' className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" />
               </div>
               </form>
-              <h2 className="xs:text-left sm:text-left md:text-left font-bold text-gray-700 mt-5 mb-5">You can log in here:</h2>
+              <h2 className="xs:text-left sm:text-left md:text-left font-bold text-gray-700 mt-5 mb-5">You can log in here</h2>
               <form className="space-y-6" autoComplete="off" onSubmit={signIn}>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mt-4">
