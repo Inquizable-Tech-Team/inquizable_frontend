e4f72fae1ffb2af051e6075e795560a80c57e5fe
Lodos <o.ueguer@gmail.com>
2021-07-07 11:38:26 +0200

    logo clickable

--- src/components/Login.js
+++ src/components/Login.js
@@ -51,7 +51,9 @@ const register = (e) => {
     return (
       <div className="min-h-screen bg-white flex">
       <div className="hidden lg:block relative w-0 flex-1">
+        <a href="/home">
       <img className="absolute inset-0 h-full w-full object-cover" src={BrainIMG} alt=""/>
+      </a>
     </div>
     <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
       <div className="mx-auto w-full max-w-sm lg:w-96">
