7d76b5898def002bb6421b1a51cec97198059d34
Lodos <o.ueguer@gmail.com>
2021-07-07 11:21:35 +0200

    few changes on login and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,20 +7,20 @@ const Footer = () => {
       <section className="bg-white w-100vw">
         <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8  xs:overflow-x-hidden">
           <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+          <div className="px-5 py-2">
+              <a
+                href="/home"
+                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+              >
+                Home
+              </a>
+            </div>
             <div className="px-5 py-2">
               <a
                 href="/about"
                 className="text-base leading-6 text-gray-500 hover:text-gray-900"
               >
                 About
-              </a>
-            </div>
-            <div className="px-5 py-2">
-              <a
-                href="/home"
-                className="text-base leading-6 text-gray-500 hover:text-gray-900"
-              >
-                Blog
               </a>
             </div>
             <div className="px-5 py-2">
